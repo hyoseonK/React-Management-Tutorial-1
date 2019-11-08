f1bd3da6d85797d6e3821b19e5de86a596ccd475
hyoseonK <gytjs825@gmail.com>
2019-11-09 03:36:31 +0900

    Apply Meterial UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';//컴포넌트의 외부를 감싸기 위한 컴포넌트
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({ 
+  //너비가 100프로, 너비 여백을 3의 가중치만큼, x쪽으로 오토를 넣어줌. 
+  //테이블이 무조건1080 픽셀 이상 테이블 출력하도록 (가로 스크롤바가 생길 수 있음)
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
 const customers=[{
   'id':1,
   'image':'https://placeimg.com/64/64/1',//이미지를 랜덤으로 보여주는 웹사이트(이미지 사이즈:64*64)
@@ -29,10 +50,22 @@ const customers=[{
 
 class App extends React.Component{
   render(){
+    const { classes } = this.props; //위의 style이 적용될 수 있도록
     return(
-      <div>
-        {
-          customers.map(c => {
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {customers.map(c => {
             return (<Customer
               key={c.id}
               id={c.id}
@@ -43,9 +76,12 @@ class App extends React.Component{
               job={c.job}/>)
           })
         }
-      </div>
+          </TableBody>
+        </Table>
+      
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App); //style을 적용할 수 있도록
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,39 +1,22 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{ //한 명에 대한 정보를 출력해줌
     render(){ //항상 수행되는 내용
-        return(//{this.props.ooo}를 이용해서 분리해줌. 계층적, 구조화해서 표현 할 수 있음
-            <div> 
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+        return(
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
 
-class CustomerProfile extends React.Component{ //하나의 고객에 대한 이미지, 이름, id를 출력해주는 컴포넌트
-    //사용자의 id, 이미지값을 랜더링 하는 부분
-    //시각장애인들은 이미지를 볼 수 없기에 이미지에 대한 설명을 alt="profile"에 넣어줌
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"></img>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
 
-class CustomerInfo extends React.Component{ //하나의 고객에 대한 남은 정보를 출력해주는 컴포넌트
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 
 export default Customer;
